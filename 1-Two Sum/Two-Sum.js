5cdcbc1e90093aa3b3f53b8460188e3250f10635
Vivek Jaiswal <vivekjaiswal@Viveks-MBP.lan>
2023-11-11 18:14:18 -0600

    Javascript solution without the test case

---
+++ 1-Two Sum/Two-Sum.js
@@ -0,0 +1,25 @@
+/**** 
+ * https://www.teamblind.com/post/New-Year-Gift---Curated-List-of-Top-75-LeetCode-Questions-to-Save-Your-Time-OaM1orEU
+ * https://leetcode.com/problems/two-sum/
+ ****/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    //create a hashmap to store complement values
+    map = {};
+    for(let i=0; i<nums.length; i++) {
+        let value = nums[i];
+        let complementPair = target - value;
+        if(map[complementPair] !== undefined) {
+            return [ map[complementPair], i ];
+        } else {
+            map[value] = i;
+        }
+
+    }
+    
+};
